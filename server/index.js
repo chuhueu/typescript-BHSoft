0b91be8ad0d04092c857d0c46d935c12de3d0ec0
Hueu123 <chuhueu1@gmail.com>
2021-11-27 22:50:23 +0700

    commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoute = require("./routes/auth");
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.use(cors());
+const URL =
+  "mongodb+srv://testBoy:hieu123@cluster0.qhhal.mongodb.net/login-BHSoft?retryWrites=true&w=majority";
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected...!"))
+  .catch((err) => console.log({ message: err.message }));
+
+// MIDDLEWARE
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.get("/", (req, res) => {
+  res.send("APP IS RUNNING");
+});
+app.listen(process.env.PORT || 8800, () => {
+  console.log("Backend server is running!");
+});
---
+++ server/routes/auth.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user); //201 Create successfully
+  } catch (error) {
+    res.status(500).json(error); //500 internal server error
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username"); //401 Unauthorized
+    if (user.password !== req.body.password) {
+      res.status(401).json("Wrong password or username!");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//GET
+router.get("/", async (req, res) => {
+  try {
+    const getUser = await User.find();
+    res.status(200).json(getUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+module.exports = router;
