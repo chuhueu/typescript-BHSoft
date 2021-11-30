55190c54a14357bd30831b94986580288137b6bd
Hueu123 <chuhueu1@gmail.com>
2021-11-30 11:07:16 +0700

    commit

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://netflix-test-server.herokuapp.com/api/",
+    //baseURL: "https://netflix-test-server.herokuapp.com/api/",
+    baseURL: "https://login-bhsoft.herokuapp.com/api/",
 })
 
 export default instance;
